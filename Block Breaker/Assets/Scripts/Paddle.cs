c43b6583589e055682a54e8cf15d826457243bdc
Tyler <torrest123@gmail.com>
2020-08-22 23:04:59 -0500

    Initial Commit
    Halfway through, should've set up earlier

---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // configuration parameters
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float xMinPosition = 1f;
+    [SerializeField] float xMaxPosition = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePositionInUnits = Input.mousePosition.x /  Screen.width * screenWidthInUnits;
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mousePositionInUnits, xMinPosition, xMaxPosition);
+        transform.position = paddlePosition;
+    }
+}
