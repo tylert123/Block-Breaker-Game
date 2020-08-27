fffa6714cbf8155c795774ef26d084d7277c3798
Tyler <torrest123@gmail.com>
2020-08-27 14:44:38 -0500

    Tags added
    Tags used to make breakable and unbreakable bricks

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -4,26 +4,61 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+
+    // config params
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] int maxHits;
 
     // cached reference
     Level level;
 
+    // state variables
+    [SerializeField] int timesHit; // serialized for debugging
+
     private void Start()
     {
-        level = FindObjectOfType<Level>();
-        level.CountBreakableBlocks();
+        CountBreakableBlocks();
     }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestroyBlock();
+        if (tag == "Breakable")
+        {
+            timesHit++;
+            if (timesHit == maxHits)
+            {
+                DestroyBlock();
+            }
+        }
     }
 
     private void DestroyBlock()
     {
+            PlayBlockDestroySFX();
+            Destroy(gameObject);
+            level.BlockDestroyed();
+            TriggerSparklesVFX();
+    }
+
+    private void PlayBlockDestroySFX()
+    {
         FindObjectOfType<GameSession>().AddToScore();
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
-        Destroy(gameObject);
-        level.BlockDestroyed();
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 2f);
     }
 }
